aed940b0549900f5950447f59a92a84560870e29
fjuimpingchen <49476358+fjuimpingchen@users.noreply.github.com>
2020-09-30 10:37:55 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import ProductList from './src/product/ProductList';
+
+
+
+
+export default function App() {
+
+    return (
+
+        <View style={styles.container}>
+
+            <ProductList />
+
+        </View>
+
+    );
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        flex: 1,
+
+        backgroundColor: '#fff',
+
+        alignItems: 'center',
+
+        justifyContent: 'center',
+
+    },
+
+})
